6d43469a24f6527ce557794c4049beefb3ba2409
adagit94 <56156969+adagit94@users.noreply.github.com>
2022-07-06 16:41:06 +0200

    diagonal computation of coordinates and scale/inner circles extension

--- js/CirclesChain/CirclesChain.js
+++ js/CirclesChain/CirclesChain.js
@@ -46,6 +46,7 @@ export default class CirclesChain extends Shader {
     this.#circle.verticesCount = circle.coordinates.length / 2;
     this.#circle.instance = circle;
     this.#circle.r = r;
+    this.#circle.innerCircles = 1;
     this.#circle.spacing = {
       x: (this.gl.canvas.width / 100) * 15,
       y: (this.gl.canvas.height / 100) * 15,
@@ -68,6 +69,8 @@ export default class CirclesChain extends Shader {
       vao,
       verticesCount,
       spacing,
+      r,
+      innerCircles,
     } = this.#circle;
 
     this.gl.useProgram(program);
@@ -75,14 +78,14 @@ export default class CirclesChain extends Shader {
 
     for (
       let xOffset = spacing.x, yOffset = -spacing.y, scale = 1;
-      xOffset < this.gl.canvas.width &&
-      yOffset * -1 < this.gl.canvas.height;
-      xOffset += spacing.x - (xOffset / this.gl.canvas.width) * 16,
-        yOffset -= spacing.y + (yOffset / this.gl.canvas.height) * 16,
+      xOffset + r * scale < this.gl.canvas.width &&
+      yOffset * -1 + r * scale < this.gl.canvas.height;
+      xOffset += spacing.x - (xOffset / this.gl.canvas.width) * 10,
+        yOffset -= spacing.y - ((yOffset * -1) / this.gl.canvas.height) * 10,
         scale =
           1 -
           (xOffset / this.gl.canvas.width +
-            yOffset * -1 / this.gl.canvas.height) /
+            (yOffset * -1) / this.gl.canvas.height) /
             2
     ) {
       const mat = ShaderUtils.mult2dMats(circleMat, [
@@ -94,6 +97,23 @@ export default class CirclesChain extends Shader {
       this.gl.uniform3f(locations.color, 1, 1, 1);
 
       this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, verticesCount - 2);
+
+      for (let innerCircle = 0; innerCircle < innerCircles; innerCircle++) {
+        const innerCircleDifference = 0.1;
+        const innerCircleScale = scale * (1 - innerCircleDifference);
+        const innerCircleYTranslation =
+          yOffset + r * scale * innerCircleDifference;
+
+        const innerCircleMat = ShaderUtils.mult2dMats(circleMat, [
+          ShaderUtils.init2dTranslationMat(xOffset, innerCircleYTranslation),
+          ShaderUtils.init2dScaleMat(innerCircleScale, innerCircleScale),
+        ]);
+
+        this.gl.uniformMatrix3fv(locations.mat, false, innerCircleMat);
+        this.gl.uniform3f(locations.color, 1, 0, 0);
+
+        this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, verticesCount - 2);
+      }
     }
   }
 
