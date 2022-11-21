318f7b41420f06100d0af75c3b541435466ef1d5
Adam Pelc <adam.pelc@abra.eu>
2022-11-21 23:26:43 +0100

    extension of cylinder possibilities through parametrization

--- src/shapes/3d/Cylinder.js
+++ src/shapes/3d/Cylinder.js
@@ -1,42 +1,69 @@
 import Shape from "../Shape.js";
 
 class Cylinder extends Shape {
-    constructor(name, ctx, r, height, density) {
-        super(name, ctx, () => {
+    static #INNER_CYLINDER_SCALE = 0.95
+    
+    constructor(name, ctx, r, height, density, optionals) {
+        super(name, ctx, (instance) => {
+            const angle = optionals?.angle ?? Math.PI * 2
+            const partialAngle = instance.partialAngle = angle !== Math.PI * 2
+            const sidesOpened = instance.sidesOpened = partialAngle || !!optionals?.sidesOpened
+            const invertNormals = !sidesOpened && !!optionals?.invertNormals
+            const cylinderAngleDensity = partialAngle ? Math.round(density * (angle / (Math.PI * 2))) : density
+            const cylinderAngleStep = angle / cylinderAngleDensity
+            
             let cylinderVertices = [], cylinderNormals = []
-            let topBaseVertices = [], topBaseNormals = []
-            let bottomBaseVertices = [], bottomBaseNormals = []
+            let sidesVertices = [], sidesNormals = []
             
-            const angleStep = Math.PI * 2 / density
+            let topSidesVertices = [], topSidesNormals = []
+            let bottomSidesVertices = [], bottomSidesNormals = []
 
-            topBaseVertices.push(0, height / 2, 0)
-            topBaseNormals.push(0, 1, 0)
-            bottomBaseVertices.push(0, -height / 2, 0)
-            bottomBaseNormals.push(0, -1, 0)
-            
-            for (let v = 0; v <= density; v++) {
-                const angle = v * angleStep;
-                const cos = Math.cos(angle)
-                const sin = Math.sin(angle)
-                const x = cos * r
-                const z = sin * r
-
-                const topCoords = [x, height / 2, z]
-                const bottomCoords = [x, -height / 2, z]
+            ;(function initData(r, invertNormals) {
+                const cylinderNormalPolarity = invertNormals ? -1 : 1
+                const topSideNormal = [0, !sidesOpened && invertNormals ? -1 : 1, 0]
+                const bottomSideNormal = [0, !sidesOpened && invertNormals ? 1 : -1, 0]
                 
-                topBaseVertices.push(...topCoords)
-                topBaseNormals.push(0, 1, 0)
+                if (!sidesOpened) {
+                    topSidesVertices.push(0, height / 2, 0)
+                    topSidesNormals.push(...topSideNormal)
 
-                bottomBaseVertices.push(...bottomCoords)
-                bottomBaseNormals.push(0, -1, 0)
+                    bottomSidesVertices.push(0, -height / 2, 0)
+                    bottomSidesNormals.push(...bottomSideNormal)
+                }
+            
+                for (let v = 0; v <= density; v++) {
+                    const angle = v * cylinderAngleStep;
+                    const cos = Math.cos(angle)
+                    const sin = Math.sin(angle)
+                    const x = cos * r
+                    const z = sin * r
 
-                cylinderVertices.push(...topCoords)
-                cylinderNormals.push(cos, 0, sin)
-                cylinderVertices.push(...bottomCoords)
-                cylinderNormals.push(cos, 0, sin)
-            }
+                    const topCoords = [x, height / 2, z]
+                    const bottomCoords = [x, -height / 2, z]
+                    
+                    topSidesVertices.push(...topCoords)
+                    topSidesNormals.push(...topSideNormal)
 
-            return { vertices: [...cylinderVertices, ...topBaseVertices, ...bottomBaseVertices], normals: [...cylinderNormals, ...topBaseNormals, ...bottomBaseNormals] }
+                    bottomSidesVertices.push(...bottomCoords)
+                    bottomSidesNormals.push(...bottomSideNormal)
+
+                    cylinderVertices.push(...topCoords)
+                    cylinderNormals.push(cos * cylinderNormalPolarity, 0, sin * cylinderNormalPolarity)
+
+                    cylinderVertices.push(...bottomCoords)
+                    cylinderNormals.push(cos * cylinderNormalPolarity, 0, sin * cylinderNormalPolarity)
+                }
+
+                if (sidesOpened && !invertNormals) initData(r * Cylinder.#INNER_CYLINDER_SCALE, true) 
+            })(r, invertNormals)
+
+            sidesVertices.push(...topSidesVertices)
+            sidesNormals.push(...topSidesNormals)
+
+            sidesVertices.push(...bottomSidesVertices)
+            sidesNormals.push(...bottomSidesNormals)
+
+            return { vertices: [...cylinderVertices, ...sidesVertices], normals: [...cylinderNormals, ...sidesNormals] }
         })
 
         this.density = density
@@ -46,11 +73,11 @@ class Cylinder extends Shape {
 
     render = () => {
         const cylinderVertices = (this.density + 1) * 2
-        const baseVertices = this.density + 2
+        const sideVertices = this.density + 2
         
         this.drawArrays(this.gl.TRIANGLE_STRIP, { count: cylinderVertices})
-        this.drawArrays(this.gl.TRIANGLE_FAN, { offset: cylinderVertices, count: baseVertices })
-        this.drawArrays(this.gl.TRIANGLE_FAN, { offset: cylinderVertices + baseVertices, count: baseVertices })
+        this.drawArrays(this.gl.TRIANGLE_FAN, { offset: cylinderVertices, count: sideVertices })
+        this.drawArrays(this.gl.TRIANGLE_FAN, { offset: cylinderVertices + sideVertices, count: sideVertices })
     }
 }
 
