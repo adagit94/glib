6d0d612206026f2fd7bb2a8569f82e8aa88471ee
Adam Pelc <sikulin94@gmail.com>
2024-05-25 17:47:23 +0200

    use vec-ts from npm

--- src/Buffers/Buffers.ts
+++ src/Buffers/Buffers.ts
@@ -1,38 +1,38 @@
-import {
-  TBuffersSettings,
-  TGetBuffer,
-  TSetBuffer,
-  TBuffers,
-  TWriteBuffer,
-} from "./BuffersTypes"
+import { TBuffersSettings, TGetBuffer, TSetBuffer, TBuffers, TWriteBuffer } from "./BuffersTypes"
 
 interface IBuffers {
-  get: TGetBuffer
-  set: TSetBuffer
-  write: TWriteBuffer
+    get: TGetBuffer
+    set: TSetBuffer
+    write: TWriteBuffer
 }
 
 export class Buffers implements IBuffers {
-  constructor(settings: TBuffersSettings) {
-    this.settings = settings
-  }
+    constructor(settings: TBuffersSettings) {
+        this.settings = settings
+    }
 
-  private settings: TBuffersSettings
-  private buffers: TBuffers = {}
+    private settings: TBuffersSettings
+    private buffers: TBuffers = {}
 
-  public get: TGetBuffer = (key) => {
-    return this.buffers[key]
-  }
+    public get: TGetBuffer = (key) => {
+        return this.buffers[key]
+    }
 
-  public set: TSetBuffer = (key, descriptor) => {
-    const buffer = (this.buffers[key] = this.settings.device.createBuffer(descriptor))
+    public set: TSetBuffer = (key, descriptor) => {
+        const buffer = (this.buffers[key] = this.settings.device.createBuffer(descriptor))
 
-    return buffer
-  }
+        return buffer
+    }
 
-  public write: TWriteBuffer = (key, settings) => {
-    const buffer = this.get(key)
-    
-    this.settings.device.queue.writeBuffer(buffer, settings.bufferOffset, settings.data, settings.dataOffset, settings.size)
-  }
+    public write: TWriteBuffer = (key, settings) => {
+        const buffer = this.get(key)
+
+        this.settings.device.queue.writeBuffer(
+            buffer,
+            settings.bufferOffset,
+            settings.data,
+            settings.dataOffset,
+            settings.size
+        )
+    }
 }
