5ea1823b27470f1a62a18f9bae3085260aa7b1f2
Adam Pelc <sikulin94@gmail.com>
2024-07-27 06:38:28 +0200

    framer update

--- src/framer.ts
+++ src/framer.ts
@@ -1,9 +1,17 @@
-type TFramerSettings = { timescale: number, renderer: () => void }
+export type Renderer = (values: TFramerValues) => void
+
+export type TFramerValues = {
+    delta: number,
+    frameDelta: number,
+    lastTimestamp: number,
+}
+
+export type TFramerSettings = { renderer: Renderer, timescale?: number }
 
 export const createFramer = ({ timescale, renderer }: TFramerSettings) => {
     let anim = true
     
-    const values = {
+    let values: TFramerValues = {
         delta: 0,
         frameDelta: 0,
         lastTimestamp: 0,
@@ -13,11 +21,13 @@ export const createFramer = ({ timescale, renderer }: TFramerSettings) => {
         const now = Date.now() * (timescale ?? 1)
         const elapsedTime = values.lastTimestamp === 0 ? 0 : now - values.lastTimestamp
 
-        values.delta += elapsedTime
-        values.frameDelta = elapsedTime
-        values.lastTimestamp = now
-
-        renderer()
+        values = {
+            delta: values.delta + elapsedTime,
+            frameDelta: elapsedTime,
+            lastTimestamp: now,
+        }
+        
+        renderer(values)
 
         if (anim) window.requestAnimationFrame(render)
     }
@@ -25,9 +35,11 @@ export const createFramer = ({ timescale, renderer }: TFramerSettings) => {
     const trigger = (animate = true) => {
         anim = animate
         
-        values.delta = 0
-        values.frameDelta = 0
-        values.lastTimestamp = 0
+        values = {
+            delta: 0,
+            frameDelta: 0,
+            lastTimestamp: 0,
+        }
 
         window.requestAnimationFrame(render)
 
@@ -40,5 +52,5 @@ export const createFramer = ({ timescale, renderer }: TFramerSettings) => {
         }
     }
 
-    return [trigger, values]
+    return trigger
 }
